b06db9a1a267a9169f0a1f447db5b64bbac916c9
joseparra97 <jose_parra97@outlook.com>
2016-04-11 09:47:08 -0430

    backend de modificar informe

--- secretario/views.py
+++ secretario/views.py
@@ -271,6 +271,8 @@ def tarjeta(request, vista, idPub, y):
      request.session['idgrupo'] = p.FKgrupo.pk
      inf=Informe.objects.filter(year=y, FKpub=idPub).order_by('mes')
      if len(inf)>0:
+          request.session['tarjetaPub']=idPub
+          request.session['tarjetaY']=y
           datos={'pub':inf, 'p':p}
      else:
           datos={'vacio':1}
@@ -281,6 +283,42 @@ def tarjeta(request, vista, idPub, y):
           pagina = 'tarjetaPub.html'
 
      return render(request, pagina , datos)
+
+def modInf(request):
+     msg={}
+     try:
+          y=request.session['tarjetaY']
+     except(KeyError):
+          msg={'msg':'Seleccione un informe'}
+     else:
+          try:
+               p=request.session['tarjetaPub']
+          except(KeyError):
+               msg={'msg':'Seleccione un informe'}
+     mes=request.POST['mes']
+     horas=request.POST['horas']
+     revisitas=request.POST['revisitas']
+     estudios=request.POST['estudios']
+     publicaciones=request.POST['publicaciones']
+     videos=request.POST['videos']
+     try:
+          inf=Informe.objects.get(FKpub=p, year=y, mes=mes)
+     except(KeyError):
+          msg={"msg":"no existe un informe que haya sido registrado en esa fecha"}
+     else:
+          if inf.horas==horas and inf.revisitas==revisitas and inf.estudios==estudios and inf.publicaciones==publicaciones and inf.videos==videos:
+               msg={"msg":"Usted no ha realizado ningún cambio"}
+          else:
+               inf.horas=horas
+               inf.revisitas=revisitas
+               inf.estudios=estudios
+               inf.publicaciones=publicaciones
+               inf.videos=videos
+               inf.save()
+               msg={"msg":"Datos del informe modificados con exito"}
+     HttpResponse(json.dumps(msg))
+
+
 
 def conPubG(request):
      cont=0
