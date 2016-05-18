4122c1e6db8e57f42ebba029c09c237dd6380f60
joseparra97 <jose_parra97@outlook.com>
2016-05-18 12:23:53 -0430

    renombrar clase a gestion y agregado de la funcion trimeUpper

--- secretario/views/siscong.py
+++ secretario/views/siscong.py
@@ -1,4 +1,4 @@
-class validacion:
+class gestion:
     elementos={}
     nroKeys=[]
     fechaKeys=[]
@@ -13,19 +13,32 @@ class validacion:
         self.mensaje=msg
     def validar(self):
         for i in self.elementos.keys():
-            if i in self.nroKeys:
-                try:
-                    int(self.elementos[i])
-                except(ValueError):
-                    self.error=False
-                    break
-            elif i in self.fechaKeys:
-                try:
-                    datetime.date(int(self.elementos[i][0:4]), int(self.elementos[i][5:7]), int(self.elementos[i][8:]))
-                except(ValueError):
-                    self.error=False
-                    break
-            else:
-                if self.elementos[i].strip()=="":
-                    self.error=False
-                    break
+            if i not in self.ignore:
+                if i in self.nroKeys:
+                    try:
+                        int(self.elementos[i])
+                    except(ValueError):
+                        self.error=False
+                        break
+                elif i in self.fechaKeys:
+                    try:
+                        datetime.date(int(self.elementos[i][0:4]), int(self.elementos[i][5:7]), int(self.elementos[i][8:]))
+                    except(ValueError):
+                        self.error=False
+                        break
+                else:
+                    if self.elementos[i].strip()=="":
+                        self.error=False
+                        break
+
+    def trimUpper(self):
+         modElemets={}
+         for i in self.elementos.keys():
+              if i not in self.ignore:
+                   try:
+                        modElemets[i]=self.elementos[i].upper().strip()
+                   except(AttributeError):
+                        modElemets[i]=self.elementos[i]
+              else:
+                   modElemets[i]=self.elementos[i]
+         return modElemets
