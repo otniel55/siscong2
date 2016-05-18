7a423c408ed76e96db4591f957d7a25309e19480
joseparra97 <jose_parra97@outlook.com>
2016-05-18 11:57:26 -0430

    refactorizar consulta de grupos

--- secretario/views/grupos.py
+++ secretario/views/grupos.py
@@ -1,13 +1,14 @@
 #libs propios de python
 import json
+import datetime
 #modulos de django
 from django.http import HttpResponse
 from django.shortcuts import render
 #modulos propios del proyecto
-from ..forms import CrearGrupo
+from ..forms import CrearGrupo, traerGrupo, modalPub, regInforme
 from .siscong import validacion
 #modelos
-from secretario.models import GruposPred
+from secretario.models import GruposPred, Publicador
 
 def Vista_registrar(request):
      form = CrearGrupo()
@@ -33,3 +34,42 @@ def registrar(request):
      else:
           msg=validar.mensaje
      return  HttpResponse(json.dumps(msg))
+
+def vistaConsultar(request):
+     try:
+          request.session['idgrupo']
+     except KeyError:
+          cGrupo = traerGrupo()
+          on = 0
+     else:
+          fkgrupo = request.session['idgrupo']
+          if fkgrupo == "":
+               cGrupo = traerGrupo()
+               on = 0
+          else:
+               cGrupo = traerGrupo(initial={'Encargado': fkgrupo })
+               request.session['idgrupo']=""
+               on = 1
+     return render(request, 'conGrupo.html', {'form': cGrupo, 'onPub': on, 'url':1 })
+
+def conGrupoofPubs(request, idGrupo):
+     cGrupo = traerGrupo(initial={'Encargado': idGrupo})
+     return render(request, 'conGrupo.html', {'form': cGrupo, 'onPub': 1, 'url':1 })
+
+def consultar(request,idGrupo):
+     try:
+          g = GruposPred.objects.get(pk=idGrupo)
+     except(KeyError, GruposPred.DoesNotExist):
+          datos={"msg":"Este grupo no esta registrado"}
+     else:
+          request.session['conGrupoId']=idGrupo
+          p = Publicador.objects.filter(FKgrupo=g.pk)
+          formDatGrupo = CrearGrupo(instance=g)
+          formPub = modalPub()
+          modalGrupo = traerGrupo()
+          modalInfo = regInforme()
+          y=str(datetime.date.today().year-1)+""+str(datetime.date.today().year)
+          datos = {'form': formDatGrupo, 'publicadores': p, 'num': g.pk, 'modalPub': formPub,
+                 'modalGrupo': modalGrupo, 'modalInfo': modalInfo, 'y':y, 'url':1,
+                 }
+     return render(request, 'datGrupo.html',datos)
