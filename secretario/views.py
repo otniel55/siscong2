3cc8ba29f2021951ba59f419f7a1c40ebac58439
joseparra97 <jose_parra97@outlook.com>
2016-05-18 09:32:56 -0430

    error de orden de resultados de meses de informes en pdftarpub corregido

--- secretario/views.py
+++ secretario/views.py
@@ -1461,6 +1461,7 @@ def datosPdfPub(request):
                if y>0:
                     inform=Informe.objects.filter(FKpub=p.pk).order_by("year", "mes")
                     if len(inform)>0:
+                         ultimoInf=Informe.objects.filter(FKpub=p.pk).order_by("-year", "-mes")[0]
                          mesPrimerInf=inform[0].mes
                          yearPrimerInf=inform[0].year
                          meses=arrayServicio(y)
@@ -1472,23 +1473,29 @@ def datosPdfPub(request):
                               cont=0
                               data[i]={}
                               for j in meses[i]:
-                                   try:
-                                        inf=Informe.objects.get(FKpub=p.pk, mes=j[1], year=j[0])
-                                   except:
-                                        if not fin:
-                                             data[i][cont]={'vacio':1, 'mes':j[1]}
+                                   if getDiferenciaMes(j[1],j[0],ultimoInf.mes,ultimoInf.year)>-2:
+                                        try:
+                                             inf=Informe.objects.get(FKpub=p.pk, mes=j[1], year=j[0])
+                                        except:
+                                             if not fin:
+                                                  data[i][cont]={'vacio':1, 'mes':j[1]}
+                                             else:
+                                                  data[i][cont]={'mes':j[1], 'horas':"", 'publicaciones':"",
+                                                      'revisitas':"", 'estudios':"", 'videos':""
+                                                      }
                                         else:
-                                             data[i][cont]={'mes':j[1], 'horas':"", 'publicaciones':"",
-                                                 'revisitas':"", 'estudios':"", 'videos':""
-                                                 }
+                                             data[i][cont]={'mes':j[1], 'horas':inf.horas, 'publicaciones':inf.publicaciones,
+                                                      'revisitas':inf.revisitas, 'estudios':inf.estudios, 'videos':inf.videos
+                                                      }
+                                             if mesPrimerInf==j[1] and yearPrimerInf==j[0]:
+                                                  fin=True
+                                                  data[i][cont]['obs']="Primer Informe."
                                    else:
-                                        data[i][cont]={'mes':j[1], 'horas':inf.horas, 'publicaciones':inf.publicaciones,
-                                                 'revisitas':inf.revisitas, 'estudios':inf.estudios, 'videos':inf.videos
-                                                 }
-                                        if mesPrimerInf==j[1] and yearPrimerInf==j[0]:
-                                             fin=True
-                                             data[i][cont]['obs']="Primer Informe."
+                                        data[i][cont]={'mes':j[1], 'horas':"", 'publicaciones':"",
+                                                      'revisitas':"", 'estudios':"", 'videos':""
+                                                      }
                                    cont+=1
+                              data[i]=reverseDict(data[i])
                               if fin:
                                    break
                     else:
@@ -1503,22 +1510,39 @@ def arrayServicio(cant):
      cont=0
      cantM=cant*12
      meses={}
-     yActual=datetime.date.today().year-cant
-     mActual=9
-     servicio=[yActual, yActual+1]
+     yActual=datetime.date.today().year
+     mActual=datetime.date.today().month
+     if mActual<9:
+          servicio=[yActual-1, yActual]
+     else:
+          servicio=[yActual, yActual+1]
+          yActual+=1
+     mActual=8
      strServicio=str(servicio[0])+"-"+str(servicio[1])
      meses[strServicio]=[[yActual, mActual]]
      for i in range(1, cantM):
-          mActual+=1
-          if mActual==13:
-               mActual=1
-               yActual+=1
-          if mActual==9:
+          mActual-=1
+          if mActual==0:
+               mActual=12
+               yActual-=1
+          if mActual==8:
                cont+=1
                if cont==cant:
                     break
-               strServicio=str(yActual)+"-"+str(yActual+1)
+               strServicio=str(yActual-1)+"-"+str(yActual)
                meses[strServicio]=[[yActual,mActual]]
           else:
                meses[strServicio].append([yActual,mActual])
      return meses
+
+def reverseDict(dict):
+     dictReverse={}
+     keys=[]
+     cont=0
+     for i in dict.keys():
+          keys.append(i)
+     keys.reverse()
+     for i in keys:
+          dictReverse[cont]=dict[i]
+          cont+=1
+     return dictReverse
