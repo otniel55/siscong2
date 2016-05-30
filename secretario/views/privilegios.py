03208f144de2a44d5fe45cae0b60adfa2cda0553
joseparra97 <jose_parra97@outlook.com>
2016-05-30 17:53:49 -0400

    backend de asignar privilegio

---
+++ secretario/views/privilegios.py
@@ -0,0 +1,102 @@
+#libs propias de python
+import datetime
+import json
+#modulos propios de django
+from django.shortcuts import render
+from django.http import HttpResponse
+#modulos propios de proyecto
+from .siscong import *
+from secretario.models import Publicador, privilegio, privilegioPub
+
+def consultar(request):
+    return render(request, "Privilegio/consultar.html")
+
+def Vistanombrar(request):
+    hoy=datetime.date.today()
+    data={}
+    cont=0
+    p=Publicador.objects.exclude(fechaBau__startswith="No").exclude(privilegiopub__status=True)
+    for x in p:
+       fechaBau=datetime.date(int(x.fechaBau[0:4]), int(x.fechaBau[5:7]), int(x.fechaBau[8:]))
+       if getEdad(x.fechaNa, hoy)>17 and x.sexo=="M":
+           data[cont]={'pk':x.pk, 'nombre':x.nombre, 'apellido':x.apellido, 'tiempoB':getEdad(fechaBau, hoy), 'fechaBau':x.fechaBau, 'edad':getEdad(x.fechaNa, hoy)}
+           cont+=1
+    data=data.values()
+    return render(request, "Privilegio/nombrar.html", {'data':data})
+
+def consultarNombrados(request):
+    return render(request, "Privilegio/consNombrados.html")
+
+def nombrar(request):
+    cont=0
+    hoy=datetime.date.today()
+    msg={}
+    pubs=json.loads(request.POST['pub'])
+    mes=int(request.POST['fechaIni'][0:2])
+    year=int(request.POST['fechaIni'][3:])
+    validaciones=True
+    if getDiferenciaMes(int(mes), int(year),hoy.month, hoy.year)>-2:
+        for p in pubs:
+            try:
+                pub=Publicador.objects.get(pk=p['id'])
+            except(KeyError, Publicador.DoesNotExist):
+                validaciones=False
+            else:
+                try:
+                    priv=privilegio.objects.get(pk=p['privilegio'])
+                except(KeyError, privilegio.DoesNotExist):
+                    validaciones=False
+                else:
+                    if pub.fechaBau[0]!="N":
+                        try:
+                            pPriv=Publicador.objects.get(pk=p['id'], privilegiopub__status=True)
+                        except(KeyError, Publicador.DoesNotExist):
+                            if getEdad(pub.fechaNa,hoy) >= priv.edadMin:
+                                fechaBau=datetime.date(int(pub.fechaBau[0:4]), int(pub.fechaBau[5:7]), int(pub.fechaBau[8:]))
+                                if getEdad(fechaBau,hoy)>=priv.tiempoBauMin:
+                                    privs=privilegioPub.objects.filter(FKpub=pub.pk).order_by("-year", "-mes")
+                                    pasar=True
+                                    for i in privs:
+                                        if privilegioActivo(i, mes, year):
+                                            pasar=False
+                                            break
+                                    if pasar:
+                                        privP=privilegioPub(FKpub=pub, FKpriv=priv, mes=mes, year=year, responsabilidad= p['respon'], status=True, fechaFin="activo")
+                                        privP.save()
+                                        msg[cont] = {'id':p['id'], 'bien':1}
+                                    else:
+                                        msg[cont] = {'id': p['id'], 'bien':0}
+                                        validaciones=False
+                                else:
+                                    msg[cont] = {'id': p['id'], 'bien':0}
+                                    validaciones=False
+                            else:
+                                validaciones=False
+                                msg[cont] = {'id': p['id'], 'bien':0}
+                        else:
+                            validaciones=False
+                    else:
+                        validaciones=False
+            cont+=1
+
+    else:
+        msg={'msg':"No se pueden hacer nombramientos del futuro"}
+        validaciones=False
+    if validaciones:
+        msg={'msg':'Se han asignado los privilegios con exito.'}
+    return HttpResponse(json.dumps(msg))
+
+def privilegioActivo(priv, mes, year):
+     activo=False
+     if getDiferenciaMes(priv.mes, priv.year, mes, year) > -2:
+          if priv.fechaFin == "activo":
+               mesF = mes
+               yearF = year
+          else:
+
+               fechaF = priv.fechaFin
+               mesF = int(fechaF[0:2])
+               yearF = int(fechaF[3:])
+          if getDiferenciaMes(mes, year, mesF, yearF) > -2:
+               activo=True
+     return activo
