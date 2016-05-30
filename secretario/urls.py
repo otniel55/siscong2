8305c1620c4d57deb1de1bdacac72d2d323f4b29
joseparra97 <jose_parra97@outlook.com>
2016-05-30 16:12:52 -0400

    url de privilegio nombrar

--- secretario/urls.py
+++ secretario/urls.py
@@ -53,5 +53,5 @@ urlpatterns = [
     url(r'^privilegio/consultar$', views.privilegio.consultar, name='privCon'),
     url(r'^privilegio/nombrar$', views.privilegio.Vistanombrar, name='privnombrar'),
     url(r'^privilegio/consultarnombrados$', views.privilegio.consultarNombrados, name='privconsnombrados'),
-    url(r'^privnombrar$', views.privilegio.nombrar, name='nombrarpriv'),
+    url(r'^privnombrar$', views.privilegio.nombrar, name='nombrarPriv'),
 ]
